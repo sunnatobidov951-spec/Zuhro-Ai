13d9e69910ddcfe12493568ae060afd838189dc0
sunnatobidov951-spec <sunnatobidov951@gmail.com>
2026-07-10 18:55:59 +0500

    Update scraper.go

--- internal/worker/scraper.go
+++ internal/worker/scraper.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/sunnatobidov951-spec/Zuhro-Ai/internal/repository/postgres"
+	"github.com/sunnatobidov951-spec/Zuhro-Ai/internal/domain"
 )
 
 type ProductSource interface {
