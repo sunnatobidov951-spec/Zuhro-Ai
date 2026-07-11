3eae8b3d32f5c2e0cc7e9e7405542f99602fe27e
sunnatobidov951-spec <sunnatobidov951@gmail.com>
2026-07-12 00:12:07 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
+package main
+
 import (
 	"context"
 	"database/sql"
-	"fmt"
-	"log/slog"
+	"log"
 	"os"
 
 	"zuhroai/internal/repository/postgres"
-	"zuhroai/internal/worker"
 
 	_ "github.com/lib/pq"
 )
+
+func main() {
+	ctx := context.Background()
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Println("DATABASE_URL is not set")
+		return
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.PingContext(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	productRepo := postgres.NewProductRepository(db)
+
+	_ = productRepo
+
+	log.Println("Zuhro.AI server started")
+}
